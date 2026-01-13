ead6b23d97ddcadfd82649e06d08d6821adc3d9a
krunal201 <krunalatulbhai@gmail.com>
2026-01-13 19:47:44 +0530

    new file

---
+++ another/app/Http/Controllers/HideController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Crypt;
+use Illuminate\Http\Request;
+
+class HideController extends Controller
+{
+    public function enc($st){
+        $a=Crypt::encryptString($st);
+        echo "Parameter after encryption: $a";
+    }
+
+    public function dec($st){
+        $a=Crypt::decryptString($st);
+        echo "<br>Decryted value: $a";
+    }
+}
---
+++ another/resources/views/third.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data</title>
+</head>
+<body>
+    <!-- When there is no desire, all things are at peace. - Laozi -->
+    <div>
+        <a href=""></a>
+    </div>
+</body>
+</html>
