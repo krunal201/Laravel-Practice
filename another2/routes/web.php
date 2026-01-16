e6b8f5dfd26d398353d4cb5553e9da2c4f1b8fe5
krunal201 <krunalatulbhai@gmail.com>
2026-01-16 13:12:26 +0530

    Modified

--- another2/routes/web.php
+++ another2/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('Login');
+});
+
+Route::post("/check",[UserController::class,"login"])->name("check");
