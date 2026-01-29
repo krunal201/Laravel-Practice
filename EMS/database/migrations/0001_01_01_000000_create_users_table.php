c493c0f4d387a3c1a99a41c20de0655d7381ca44
krunal201 <krunalatulbhai@gmail.com>
2026-01-29 18:06:40 +0530

    Make chnages in table

--- EMS/database/migrations/0001_01_01_000000_create_users_table.php
+++ EMS/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,21 +14,19 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email',20)->unique();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('email',20)->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
 
         Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
+            $table->string('id',20)->primary();
             $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
