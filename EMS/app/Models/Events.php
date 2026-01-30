a7dad2085572bc97e9bd4c806550e90b12c3598e
krunal201 <krunalatulbhai@gmail.com>
2026-01-30 10:47:05 +0530

    Add Fillable

--- EMS/app/Models/Events.php
+++ EMS/app/Models/Events.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Events extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'category_id',
+        'date',
+        'time',
+        'location',
+        'image',
+    ];
 }
