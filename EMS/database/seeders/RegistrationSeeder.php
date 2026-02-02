d0574abcbf38cf5cd175af7719664cca545dc95b
krunal201 <krunalatulbhai@gmail.com>
2026-02-02 13:02:40 +0530

    Seed data into registration table

--- EMS/database/seeders/RegistrationSeeder.php
+++ EMS/database/seeders/RegistrationSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Registration;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class RegistrationSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Registration::create([
+            "user_id"=> 4,
+            "event_id"=>2,
+            "name"=>"Dale Lockman",
+            "email"=>"mills.mina@bins.info",
+            "phone"=>"9301285410",
+        ]);
     }
 }
