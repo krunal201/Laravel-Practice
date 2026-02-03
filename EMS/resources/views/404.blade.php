515943aff63032259b0548cee7e323cfdbe27d98
krunal201 <krunalatulbhai@gmail.com>
2026-02-03 14:29:55 +0530

    Page not found

---
+++ EMS/resources/views/404.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 Page not exists</title>
+    <link rel="stylesheet" href="{{ asset('css/notfound.css') }}">
+</head>
+
+<body>
+    <div class="section">
+        <h1 class="error">404</h1>
+        <div class="page">The page you are looking for does not exist.</div>
+        <!-- <a class="back-home" href="#!">Back to home</a> -->
+    </div>
+    <!-- <h1>Page not exists.</h1> -->
+</body>
+
+</html>
